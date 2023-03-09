d383ab1230a3ed7eb4e63d9ae2053502e899062f
Valentin B <valentin.be@protonmail.com>
2023-03-09 18:37:51 +0100

    Implement unsupported LWC/SWC instructions

---
+++ src/main/kotlin/jit/codegen/impl/Coprocessor.kt
@@ -0,0 +1,28 @@
+package io.github.vbe0201.jiffy.jit.codegen.impl
+
+import io.github.vbe0201.jiffy.cpu.ExceptionKind
+import io.github.vbe0201.jiffy.jit.codegen.InstructionMeta
+import io.github.vbe0201.jiffy.jit.codegen.Status
+import io.github.vbe0201.jiffy.jit.codegen.jvm.BytecodeEmitter
+
+// TODO: COP2 supports these.
+
+/** Emits an unsupported Load Word Coprocessor (LWC) to the code buffer. */
+fun lwc(meta: InstructionMeta, emitter: BytecodeEmitter): Status {
+    emitter.exception(
+        meta.exceptionPc(),
+        meta.branchDelaySlot,
+        ExceptionKind.COPROCESSOR_ERROR
+    )
+    return Status.TERMINATE_BLOCK
+}
+
+/** Emits an unsupported Store Word Coprocessor (SWC) to the code buffer. */
+fun swc(meta: InstructionMeta, emitter: BytecodeEmitter): Status {
+    emitter.exception(
+        meta.exceptionPc(),
+        meta.branchDelaySlot,
+        ExceptionKind.COPROCESSOR_ERROR
+    )
+    return Status.TERMINATE_BLOCK
+}
